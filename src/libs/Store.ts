c4ce573ea17727b8c2863e279a8320a6dd228587
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-09-16 09:52:07 +0700

    done

--- src/libs/Store.ts
+++ src/libs/Store.ts
@@ -5,13 +5,23 @@ interface MarathonFormState {
   plan: "funrun" | "mini" | "half" | "full";
   gender: "male" | "female";
   email: string;
+  haveCoupon: boolean;
+  couponCode: string;
+  password: string;
+  cfpassword: string;
+  total: number;
   // Setters
   setFname: (v: string) => void;
   setLname: (v: string) => void;
   setPlan: (v: MarathonForm["plan"]) => void;
   setGender: (v: MarathonForm["gender"]) => void;
   setEmail: (v: string) => void;
+  sethaveCoupon: (v: boolean) => void;
+  setCouponCode: (v: string) => void;
+  setPassword: (v: string) => void;
+  setCfPassword: (v: string) => void;
   // Function ชื่อ discountCupon คำนวณ total ตรงนี้
+  discountCoupon: () => void;
   reset: () => void;
 }
 export type { MarathonFormState }

ebaaa1049ead8af38f9f07322387510bed735758
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-09-16 09:51:47 +0700

    done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,10 +38,10 @@ export default function Sidebar() {
             color="red"
             withBorder
           >
-            <Avatar size="md" radius="xl" src="/charliebrown.jpg" />
+            <Avatar size="md" radius="xl" src="/angry nose.png" />
           </Indicator>
           <Text component={RouterNavLink} to="/">
-            User : Chanadda : Admin
+            User : Nontapat : Super Saiyan
           </Text>
         </Group>
       </Box>
